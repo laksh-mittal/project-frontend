4b580e1d01b8bdbc63a9b4467ac9383a4288e1d7
Laksh Mittal <laksh@Lakshs-MacBook-Pro.local>
2023-02-19 23:52:57 +0530

    updated index.js, replaced BrowserRouter by HashedRouter

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from "./App";
 import store, { persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <Router>
+      <HashRouter>
         <App />
-      </Router>
+      </HashRouter>
     </PersistGate>
   </Provider>
 );
